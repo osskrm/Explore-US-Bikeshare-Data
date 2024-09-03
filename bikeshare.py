d91f32344ccfc37417f6d03eae61efd97e14c6d8
Osama Karam <osamakaram93@gmail.com>
2024-09-03 13:53:08 +0300

    Rename Variables for Consistency in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -5,3 +5,12 @@ while city not in valid_cities:
         city = city_map[city]
     else:
         print("Sorry, input city is not in the list!")
+
+
+
+
+total_travel_time = df['Trip Duration'].sum()
+print('Total travel time:', total_travel_time / 86400, " Days")
+
+average_travel_time = df['Trip Duration'].mean()
+print('Average travel time:', average_travel_time / 60, " Minutes")
