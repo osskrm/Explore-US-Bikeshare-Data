c283aee9eb4315a9611b5f95329362937c755175
Osama Karam <osamakaram93@gmail.com>
2024-09-03 13:46:34 +0300

    Simplify User Input Validation in bikeshare.py

---
+++ bikeshare.py
@@ -0,0 +1,7 @@
+city = ''
+while city not in valid_cities:
+    city = input("\nPlease choose a city you would like to see the data for: Chicago, New York City, or Washington?\n").lower()
+    if city in valid_cities:
+        city = city_map[city]
+    else:
+        print("Sorry, input city is not in the list!")
